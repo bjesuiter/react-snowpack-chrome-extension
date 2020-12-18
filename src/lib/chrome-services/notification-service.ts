cd8d50f1d11cfeec4117e94c8c7afa6ccdb2b984
Benjamin Jesuiter <benjamin.jesuiter@flavia-it.de>
2020-12-18 22:40:00 +0100

    add basic ts adapter for chrome notifications

---
+++ src/lib/chrome-services/notification-service.ts
@@ -0,0 +1,25 @@
+async function basicNotification(title: string, message: string, iconUrl: string) {
+	return new Promise((resolve) => {
+		const notification = {
+			type: 'basic',
+			iconUrl: iconUrl,
+			title,
+			message,
+		};
+		chrome.notifications.create(notification, resolve);
+	});
+}
+
+/**
+ *
+ * Chrome Docs for NotificationOptions
+ * @param {*} text
+ * @returns {Promise<string>} resolves with the id of the notification
+ */
+export async function showSimpleNotification(title: string, message: string) {
+	return basicNotification(title, message, chrome.extension.getURL('assets/img/icon-512.png'));
+}
+
+export async function showError(title: string, message: string) {
+	return basicNotification(title, message, chrome.extension.getURL('assets/icons/error.png'));
+}
