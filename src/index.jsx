05803b07110e6857aa7035d1b6aa0256dc963023
Benjamin Jesuiter <benjamin.jesuiter@flavia-it.de>
2020-12-18 20:51:18 +0100

    import custom react component as of tutorial

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div>"HELLO REACT"</div>, document.getElementById('root'));
+import App from './App.jsx';
+ ReactDOM.render(
+   <React.StrictMode>
+     <App />
+   </React.StrictMode>,
+   document.getElementById('root'),
+ );
